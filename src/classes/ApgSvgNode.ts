c171296f870b2a03c3b8c26f8398c53d58722941
Paolo Angeli <pangeli70@gmail.com>
2023-01-22 22:28:13 +0100

    Stop Gradient syntax error

--- src/classes/ApgSvgNode.ts
+++ src/classes/ApgSvgNode.ts
@@ -201,7 +201,7 @@ export class ApgSvgNode {
       this.type === eApgSvgNodeTypes.RADIAL_GRADIENT
     ) {
       const inner =
-        `<stop offset="${astop.offset}% stop-color="${astop.color}" stop-opacity="${astop.opacity || 1}" />`;
+        `<stop offset="${astop.offset}%" stop-color="${astop.color}" stop-opacity="${astop.opacity || 1}" />`;
 
       this._innerContent.push(inner);
     }
