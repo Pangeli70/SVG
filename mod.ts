0398a997125683f39a25357f7f548c488c4b774a
Paolo Angeli <pangeli70@gmail.com>
2023-02-07 00:53:14 +0100

    Mod.ts

--- mod.ts
+++ mod.ts
@@ -7,6 +7,7 @@
  * @version 0.8.0 [APG 2022/04/03] Porting to Deno
  * @version 0.9.2 [APG 2022/11/24] Github beta
  * @version 0.9.3 [APG 2022/12/26] Paths management
+ * @version 0.9.4 [APG 2023/02/04] Hatch builder
  * -----------------------------------------------------------------------
  */
 
